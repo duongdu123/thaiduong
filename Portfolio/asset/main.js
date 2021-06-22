9af4249784e64edaab2dacc515937d87fe04e1df
duongdu123 <55027398+duongdu123@users.noreply.github.com>
2021-06-23 03:31:45 +0700

    Add files via upload

---
+++ Portfolio/asset/main.js
@@ -0,0 +1,22 @@
+window.addEventListener('scroll',function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 0);
+});
+// function toggleMenu(){
+//     var menuToggle = document.querySelector('.toggle');
+//     var menu = document.querySelector('.menu');
+//     menuToggle.classList.toggle('active');
+    
+//     menu.classList.toggle('active')
+// }
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.header-container');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+
+
+// slider carousel
+
+
